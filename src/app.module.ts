093a9d6259036a24a0279e3cf80c771656937da9
JoaoGati <jvcembranelli@gmail.com>
2022-12-15 12:15:54 -0300

    New_Changes
    New changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { HttpModule } from './http.module';
-import { MailService } from './mail/mail.service';
-import { PostMarkMailService } from './mail/postmark-mail.service';
+import { PrismaService } from './prisma.service';
 
 // import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
-  imports: [
-    HttpModule
-  ],
-  controllers: [AppController],
-  providers: [
-    {
-      provide: MailService,
-      useClass: PostMarkMailService,
-    },
-  ],
+  imports: [ HttpModule ],
+  controllers: [ AppController ],
+  providers: [ PrismaService ],
 })
 export class AppModule {}
